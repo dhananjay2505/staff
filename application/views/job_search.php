12d6cd067b7e26a7e1c7fd1d33055935f48a6e6c
dhananjay2505 <kumar.dhananjay2505@gmail.com>
2020-06-23 12:43:39 +0530

    success

--- application/views/job_search.php
+++ application/views/job_search.php
@@ -1,3 +1,248 @@
-<h1>hjkl</h1>
 
-fghgjhk
+<div class="container main_wrapper">
+<div class="col-md-4 float-left col-12">
+
+<div class="sidebar mt_20 desktop_view float-left">
+
+    <span class="section_divider"></span>
+    <div class="col-12 col-md-12 float-left mt_20">
+        <!-- <label for="exampleInputEmail1">Email address</label> -->
+        <input type="email" class="form-control search_box" id="" placeholder="Search by Job title">
+    </div> 
+
+    <div class="accordion" id="accordionExample">
+    <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          State
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        <ul class="check_list">
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_1">
+                    <label class="form-check-label" for="ch_1">
+                        Karnataka
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_2">
+                    <label class="form-check-label" for="ch_2">
+                        Goa
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_3">
+                    <label class="form-check-label" for="ch_3">
+                        Maharastra
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_4">
+                    <label class="form-check-label" for="ch_4">
+                        Delhi
+                    </label>
+                </div>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Location
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        <ul class="check_list">
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_1">
+                    <label class="form-check-label" for="ch_1">
+                        Bangalore
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_2">
+                    <label class="form-check-label" for="ch_2">
+                        Bangalore
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_3">
+                    <label class="form-check-label" for="ch_3">
+                        Hydrabad
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_4">
+                    <label class="form-check-label" for="ch_4">
+                        Delhi
+                    </label>
+                </div>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          Jobs by Industry
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+      <ul class="check_list">
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                    <label class="form-check-label" for="defaultCheck1">
+                    EVENTS & CATERING
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                    <label class="form-check-label" for="defaultCheck2">
+                        Pubs & Bar
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                    <label class="form-check-label" for="defaultCheck2">
+                        Hotel and resorts
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                    <label class="form-check-label" for="defaultCheck2">
+                        Bakary
+                    </label>
+                </div>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Salary
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        <ul class="check_list">
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_1">
+                    <label class="form-check-label" for="ch_1">
+                        0-1 Lakhs
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_2">
+                    <label class="form-check-label" for="ch_2">
+                    2-3 Lakhs
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_3">
+                    <label class="form-check-label" for="ch_3">
+                    3-4 Lakhs
+                    </label>
+                </div>
+            </li>
+            <li>
+                <div class="form-check check_list">
+                    <input class="form-check-input" type="checkbox" value="" id="ch_4">
+                    <label class="form-check-label" for="ch_4">
+                    4-5 Lakhs
+                    </label>
+                </div>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+    
+
+</div>
+</div> <!-- ===  OL_MD 4 right side section end here ========== -->
+    <div class="col-md-8 float-left col-12 no-padding">
+        <div class="wrapper">
+            <h1 class="font_14 pt_20 float-left"> 1 - 20 of 1141 Ux Design Jobs </h1>    
+            <div class="dropdown float-right">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sort by: {{Value}}
+                </a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="#">Relevance</a>
+                    <a class="dropdown-item" href="#">Recently</a>
+                    <a class="dropdown-item" href="#">Last 20 days</a>
+                    <a class="dropdown-item" href="#">Last 20 days</a>
+                </div>
+            </div>
+        <ul class="jobs"> 
+            <li>
+                <a href="">
+                    <div class="job_container">
+                        <div class="job_img"> <img src="<?php echo base_url('assets/images/job_3.png'); ?>"> </div>
+                        <div class="job_caption">
+                            <span class="primery_title"> Chedhhh </span>
+                            <span class="second_subtitle"> Burgerkhing </span>
+                            <ul class="service_tag">
+                                <li> <span class="icon_set icon-placeholders-1 mr_5"></span>    Bangalore </li>
+                                <li>2 days Ago </li>
+                                <li>Part time </li>
+                            </ul>
+                        </div>
+                    </div>
+                </a>
+            </li>
+    </ul> 
+
+        </div>
+    </div><!-- ====  COL_MD 8 right side section end here ========== -->
+    
+    
+</div>
+
